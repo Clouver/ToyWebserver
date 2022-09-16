18c530a9f972dacc1c2002bed00e7e3c291354c4
Clouver <clouvercool@gmail.com>
2022-09-16 14:44:17 +0800

    elegant exit with Timer info

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ void handleSigInt(int sig){
     if (sig == SIGINT){
         cout<<"Exit"<<endl;
         serverForSig->getLoop()->stop();
-        serverForSig.reset();
-        _exit(0);
+//        serverForSig.reset();
+//        _exit(0);
     }
 }
 
@@ -35,6 +35,5 @@ int main(int argc, char**argv){
     signal(SIGINT, [](int sig){
         return handleSigInt(sig);
     }); // SIGINT 信号由 InterruptKey 产生，通常是 CTRL +C 或者 DELETE
-
     serverForSig->start();
 }
